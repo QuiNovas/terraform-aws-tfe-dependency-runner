c41993fbcc733d4bff9600bad7a3038e92b01ea5
Joseph Wortmann <joseph.wortmann@gmail.com>
2019-03-16 17:17:12 -0500

    another test

--- main.tf
+++ main.tf
@@ -48,9 +48,30 @@ module "tfe_dependency_runner" {
 }
 
 resource "aws_api_gateway_rest_api" "tfe_dependency_runner" {
-  body        = "${data.template_file.swagger.rendered}"
   description = "Webhooks gateway for ${var.name_prefix}tfe-dependency-runner"
   name        = "${var.name_prefix}tfe-dependency-runner"
+}
+
+resource "aws_api_gateway_resource" "tfe_dependency_runner" {
+  rest_api_id = "${aws_api_gateway_rest_api.tfe_dependency_runner.id}"
+  parent_id   = "${aws_api_gateway_rest_api.tfe_dependency_runner.root_resource_id}"
+  path_part   = "tfe-dependency-runner"
+}
+
+resource "aws_api_gateway_method" "POST" {
+  rest_api_id   = "${aws_api_gateway_rest_api.tfe_dependency_runner.id}"
+  resource_id   = "${aws_api_gateway_resource.tfe_dependency_runner.id}"
+  http_method   = "POST"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "tfe_dependency_runner" {
+  rest_api_id             = "${aws_api_gateway_rest_api.tfe_dependency_runner.id}"
+  resource_id             = "${aws_api_gateway_resource.tfe_dependency_runner.id}"
+  http_method             = "${aws_api_gateway_method.POST.http_method}"
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = "${module.tfe_dependency_runner.invoke_arn}"
 }
 
 resource "aws_api_gateway_deployment" "tfe_dependency_runner" {
