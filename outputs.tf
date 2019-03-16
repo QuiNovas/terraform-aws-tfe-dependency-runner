335fd8671f7736ec9ed06b93d210a02da67c9611
Joseph Wortmann <joseph.wortmann@gmail.com>
2019-03-16 15:31:02 -0500

    fixed outputs

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,9 @@
+output "invoke_url" {
+  description = "The URL to invoke the TFE Dependency runner api"
+  value = "${aws_api_gateway_deployment.tfe_dependency_runner.invoke_url}/tfe-dependency-runner"
+}
+
 output "notification_token" {
   description = "The generated notification token for use in TFE"
-  value = "${random_string.notification_token.result}
+  value = "${random_string.notification_token.result}"
 }
